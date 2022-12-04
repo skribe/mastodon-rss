0b06abd4b7c053492869f59c10fe0ffd958f08fa
skribe <skribeproductions@gmail.com>
2022-12-04 14:02:00 +0800

    Even more conversions

--- Boilerplate/public/partials/mastodon-rss-public-display.php
+++ Boilerplate/public/partials/mastodon-rss-public-display.php
@@ -5,11 +5,10 @@
  *
  * This file is used to markup the public-facing aspects of the plugin.
  *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/public/partials
+ * @link            https://github.com/skribe/mastodon-rss
+ * @since           0.1.0
+ * @package         Mastodon_RSS
+ * @subpackage 		Mastodon_RSS/partials
  */
 ?>
 
