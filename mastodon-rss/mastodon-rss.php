0f9c04d50fa6a6acb1e4be09dbb57eabd7f4a834
skribe <skribeproductions@gmail.com>
2022-12-04 15:04:41 +0800

    A minor hiccough fixed

--- mastodon-rss/mastodon-rss.php
+++ mastodon-rss/mastodon-rss.php
@@ -5,7 +5,7 @@
  * Description:       A plugin to properly format mastodon rss feeds to be displayed in Wordpress.
  * Version:           1.0.0
  * Requires at least: 6.1
- * Requires PHP:      8.1
+ * Requires PHP:      7.4
  * Author:            skribe
  * Author URI:        https://github.com/skribe
  * License:           GPL v3
