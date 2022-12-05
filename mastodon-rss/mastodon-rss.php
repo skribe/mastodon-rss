52c78fbec1b19c0846563ee49e1e886de4865c82
skribe <skribeproductions@gmail.com>
2022-12-05 19:58:38 +0800

    New Updated versioning for new branch

--- mastodon-rss/mastodon-rss.php
+++ mastodon-rss/mastodon-rss.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Mastodon RSS
  * Plugin URI:        https://github.com/skribe/mastodon-rss
  * Description:       A plugin to properly format mastodon rss feeds to be displayed in Wordpress.
- * Version:           1.0.1
+ * Version:           1.1.0
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Author:            skribe
